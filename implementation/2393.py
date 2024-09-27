25bdd89e5c27d7757e190fc4204cefd82a66ef73
tjddnr9553 <tjddnr9553@naver.com>
2024-09-27 21:52:23 +0900

    0927 algorithm commit

---
+++ implementation/2393.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+print("  ___  ___  ___")
+print("  | |__| |__| |")
+print("  |           |")
+print("   \\_________/")
+print("    \\_______/")
+print("     |     |")
+print("     |     |")
+print("     |     |")
+print("     |     |")
+print("     |_____|")
+print("  __/       \\__")
+print(" /             \\")
+print("/_______________\\")
