5ffedd0d3733d477627c7fb0ba635fb5d59e281d
tjddnr9553 <tjddnr9553@naver.com>
2024-09-30 22:39:04 +0900

    0930 algorithm commit

--- implementation/15784.py
+++ implementation/15784.py
@@ -2,19 +2,17 @@ import sys
 
 input = sys.stdin.readline
 
-
-
 a, b, c = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(a)]
-flag = False
+flag = True
 
 for y in range(a):
-    if graph[b-1][c-1] > graph[y][c-1]:
-            flag = True
+    if graph[b - 1][c - 1] < graph[y][c - 1]:
+        flag = False
 
 for x in range(a):
-    if graph[b-1][c-1] > graph[b-1][x]:
-            flag = True
+    if graph[b - 1][c - 1] < graph[b - 1][x]:
+        flag = False
 
 if flag:
     print("HAPPY")
